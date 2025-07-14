4a65a858a872ef20e16f117b8046977e4ed80d2b
mdcowan <41978259+mdcowan@users.noreply.github.com>
2025-07-13 20:46:08 -0400

    Chapter 2, Lesson 2

--- main.py
+++ main.py
@@ -7,9 +7,34 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
+    # initializing imported module
+    pygame.init()
+
+    # Setting name for the game window
+    pygame.display.set_caption('Asteroids')
+
+    # creating a bool value which checks 
+    # if the game is running
+    running = True
+
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    # keep game running till running is true
+    while True:  
+        # Check for event if user has pressed te close button
+        for event in pygame.event.get():            
+            # if event is of type quit then exit the program
+            if event.type == pygame.QUIT:
+                return
+        
+        # fill the screen with a solid "black" color.
+        screen.fill("black")
+        
+        # refresh the screen
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
